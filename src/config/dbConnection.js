861e07c1c1f27cc853aec6a83daa04376a11ccbc
jekotasek <josekotasek@gmail.com>
2018-11-19 17:42:13 -0600

    Updated db connection

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: 'apeproducciones.com',
-    user: "apemktco_temp",
-    password: "Temporal00",
-    database : 'apemktco_aplicacionesweb'
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: "b1d70906bb051b",
+    password: "ce0d0ee6",
+    database : 'heroku_02e2090c5af5a0f'
   })
 };
