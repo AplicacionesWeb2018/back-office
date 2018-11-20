dde8c1dd5791394b5b95cdfe8d68589b992f02c3
jekotasek <josekotasek@gmail.com>
2018-11-19 19:09:04 -0600

    Updated db connection

--- src/app/routes/products.js
+++ src/app/routes/products.js
@@ -4,27 +4,24 @@ module.exports = app => {
   const connection = dbConnection();
   console.log("CONNECTED");
 
-  connection.connect(function(err) {
+  /* connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-  });
+  }); */
 
-  function handleDisconnect() {
-                                                    // the old one cannot be reused.
-  
-    connection.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
+  function handleDisconnect() {  
+    /* connection.connect(function(err) {            
+      if(err) {                                    
         console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
+        setTimeout(handleDisconnect, 2000); 
+      }                                     
+    });   */                                  
     connection.on('error', function(err) {
       console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        handleDisconnect();                        
+      } else {                                      
+        throw err;                                 
       }
     });
   };
